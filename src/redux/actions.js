d6cc12e2ed9b05ffee0e335f3cd4e35536715d6f
Olena-Zhyzhko <asil198609@gmail.com>
2022-10-27 20:52:22 +0300

    delite zero files

--- src/redux/actions.js
+++
@@ -1,7 +0,0 @@
-import { createAction } from '@reduxjs/toolkit'
-
-const addContact = createAction('contacts/addContact');
-
-const removeContact = createAction('contacts/removeContact')
-
-export { addContact, removeContact }
